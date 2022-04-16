4b915120ae6aa4a7222e69776cc2638647eef2ef
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-04-16 08:14:26 +0200

    itel-P13-Plus: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: 0 Manufacturer: itel Model: itel P13 Plus Script version: 2.1.0 Signed-off-by: Sebastiano Barezzi <barezzisebastiano@gmail.com>

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_itel-P13-Plus.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_itel-P13-Plus-user \
+    omni_itel-P13-Plus-userdebug \
+    omni_itel-P13-Plus-eng
---
+++ omni_itel-P13-Plus.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit from itel-P13-Plus device
+$(call inherit-product, device/itel/itel-P13-Plus/device.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := itel-P13-Plus
+PRODUCT_NAME := omni_itel-P13-Plus
+PRODUCT_BRAND := Itel
+PRODUCT_MODEL := itel P13 Plus
+PRODUCT_MANUFACTURER := itel
